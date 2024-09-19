444e03d3ebadeaaf9ca76a14503d82d5e3bb146b
Aviz85 <avizmaeir@gmail.com>
2024-09-19 22:20:38 +0300

    db fix 2

--- school_manage/settings.py
+++ school_manage/settings.py
@@ -90,7 +90,7 @@ if os.getenv('DATABASE_URL'):
         'default': dj_database_url.config(
             default=os.getenv('DATABASE_URL'),
             conn_max_age=600,
-            conn_health_checks=True,
+            # Remove the conn_health_checks argument
         )
     }
 else:
